9da7361a90d2b49e18de6c35d24b8fe8f31ab2b1
Steopa2001 <moraru495@gmail.com>
2025-08-25 16:38:34 +0200

    feat: start main

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,10 +1,12 @@
 //Importazione delle componenti
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <>
-      <Header />
+      <Header/>
+      <Main/>
     </>
   );
 }
---
+++ react-dc-comics/src/components/Main.jsx
@@ -0,0 +1,24 @@
+
+
+const Main = () => {
+    return (
+        <main>
+            <div className="cont-black">
+                <div className="container-black">
+                    <div className="main-text">
+                         <h2>__&gt; Content goes here &lt;__</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="cont-blue">
+                <div className="container-blue">
+                    <div className="list-icon">
+                        
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+};
+
+export default Main;
