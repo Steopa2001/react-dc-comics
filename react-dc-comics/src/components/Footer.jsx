5e6ed505d18658d8c00c8d8979d3fa7a4fbf2ead
Steopa2001 <moraru495@gmail.com>
2025-08-25 17:30:54 +0200

    feat: import the bg footer with style inline

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -1,6 +1,20 @@
+import footerBg from "../assets/img/footer-bg.jpg";
 
 const Footer = () => {
-  
+  return (
+    <footer>
+      <div
+        className="cont-foot-background"
+        style={{
+          backgroundImage: `url(${footerBg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "200px",
+        }}
+      ></div>
+      <div className="cont-foot-grey"></div>
+    </footer>
+  );
 };
 
 export default Footer;
