f51d72a453dbba5b8d9af825457d0db90120c9a4
Steopa2001 <moraru495@gmail.com>
2025-08-25 17:52:21 +0200

    feat: complete footer list

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+//importo l'immagine 
 import footerBg from "../assets/img/footer-bg.jpg";
 
 const Footer = () => {
@@ -9,10 +10,49 @@ const Footer = () => {
           backgroundImage: `url(${footerBg})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
-          height: "200px",
         }}
-      ></div>
+      >
+        <div className="list-footer">
+            <ul>
+                <li className="bold-li">DC COMICS</li>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+                <li className="bold-li">SHOP</li>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+            </ul>
+            <ul className="margin-ul">
+                <li className="bold-li">DC</li>
+                <li>Terms Of Use</li>
+                <li>Privacy policy &#40;New&#41;</li>
+                <li>Ad Choises</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+            <ul className="margin-ul">
+                <li className="bold-li">SITES</li>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+            </ul>
+        </div>
+      </div>
+
       <div className="cont-foot-grey"></div>
+
     </footer>
   );
 };
