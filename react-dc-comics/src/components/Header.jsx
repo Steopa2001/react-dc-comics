f996c7f8b3b67cb1cca101049198bcaf44ca021d
Steopa2001 <moraru495@gmail.com>
2025-08-25 16:10:50 +0200

    feat: complete header

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -4,7 +4,43 @@ import logo from "../assets/img/favicon.png";
 const Header = () => {
   return (
     <header>
-      <img src={logo} alt="logo" />
+      <div className="container">
+        <img src={logo} alt="logo" />
+        <div className="list-header">
+          <ul>
+            <li>
+              <a href="#">CHARACTERS</a>
+            </li>
+            <li>
+              <a href="#">COMICS</a>
+            </li>
+            <li>
+              <a href="#">MOVIES</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">GAMES</a>
+            </li>
+            <li>
+              <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+              <a href="#">VIDEOS</a>
+            </li>
+            <li>
+              <a href="#">FANS</a>
+            </li>
+            <li>
+              <a href="#">NEWS</a>
+            </li>
+            <li>
+              <a href="#">SHOP</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 };
