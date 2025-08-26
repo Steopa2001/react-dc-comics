ce8ddcd63d93e285ecee26680b1061044d29c885
Steopa2001 <moraru495@gmail.com>
2025-08-26 19:12:34 +0200

    feat: complete bonus

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -1,23 +1,20 @@
-
-const Footer = () => {
-
-  //creo struttura dati per i link del footer
-  const footerLinks = [
-  {
-    title: "DC COMICS",
-    links: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News", "Shop DC", "Shop DC Collectibles"]
-  },
-  {
-    title: "DC",
-    links: ["Terms Of Use", "Privacy Policy (New)", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"]
-  },
-  {
-    title: "SITES",
-    links: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"]
-  }
+//creo struttura dati per i link del footer
+const footerLinks = [
+{
+  title: "DC COMICS",
+  links: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News", "Shop DC", "Shop DC Collectibles"]
+},
+{
+  title: "DC",
+  links: ["Terms Of Use", "Privacy Policy (New)", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"]
+},
+{
+  title: "SITES",
+  links: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"]
+}
 ];
 
-
+const Footer = () => {
 
   return (
     <footer>
@@ -29,41 +26,14 @@ const Footer = () => {
         ></div>
 
         <div className="list-footer">
-          <ul>
-            <li className="bold-li">DC COMICS</li>
-            <li>Characters</li>
-            <li>Comics</li>
-            <li>Movies</li>
-            <li>TV</li>
-            <li>Games</li>
-            <li>Videos</li>
-            <li>News</li>
-            <li className="bold-li">SHOP</li>
-            <li>Shop DC</li>
-            <li>Shop DC Collectibles</li>
-          </ul>
-          <ul className="margin-ul">
-            <li className="bold-li">DC</li>
-            <li>Terms Of Use</li>
-            <li>Privacy policy &#40;New&#41;</li>
-            <li>Ad Choises</li>
-            <li>Advertising</li>
-            <li>Jobs</li>
-            <li>Subscriptions</li>
-            <li>Talent Workshops</li>
-            <li>CPSC Certificates</li>
-            <li>Ratings</li>
-            <li>Shop Help</li>
-            <li>Contact Us</li>
-          </ul>
-          <ul className="margin-ul">
-            <li className="bold-li">SITES</li>
-            <li>DC</li>
-            <li>MAD Magazine</li>
-            <li>DC Kids</li>
-            <li>DC Universe</li>
-            <li>DC Power Visa</li>
-          </ul>
+          {footerLinks.map((section, index) => (
+            <ul key={index} className={index !== 0 ? 'margin-ul' : ''}>
+              <li className="bold-li">{section.title}</li>
+              {section.links.map((link, i) => (
+                <li key={i}>{link}</li>
+              ))}
+            </ul>
+          ))}
         </div>
       </div>
 
