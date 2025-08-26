d8073d6ff39886894dfec8589b795068ab0f8363
Steopa2001 <moraru495@gmail.com>
2025-08-26 15:40:01 +0200

    feat: create a new file in components 'Navbar.jsx'

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,46 +1,12 @@
-//importo immagine in react
-import logo from "../assets/img/dc-logo.png";
+
+//importo la Navbar 
+import Navbar from "./Navbar";
+
 
 const Header = () => {
   return (
     <header>
-      <div className="container">
-        <img src={logo} alt="logo" />
-        <div className="list-header">
-          <ul>
-            <li>
-              <a href="#">CHARACTERS</a>
-            </li>
-            <li>
-              <a href="#" className="border">COMICS</a>
-            </li>
-            <li>
-              <a href="#">MOVIES</a>
-            </li>
-            <li>
-              <a href="#">TV</a>
-            </li>
-            <li>
-              <a href="#">GAMES</a>
-            </li>
-            <li>
-              <a href="#">COLLECTIBLES</a>
-            </li>
-            <li>
-              <a href="#">VIDEOS</a>
-            </li>
-            <li>
-              <a href="#">FANS</a>
-            </li>
-            <li>
-              <a href="#">NEWS</a>
-            </li>
-            <li>
-              <a href="#">SHOP</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Navbar/>
     </header>
   );
 };
